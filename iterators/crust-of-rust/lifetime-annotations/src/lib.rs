ff8ec624c52aebe3163b970abfd8f7072090889a
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-05-05 13:54:07 -0300

    changing delimiter to be generic, and implement it for char and &str

--- iterators/crust-of-rust/lifetime-annotations/src/lib.rs
+++ iterators/crust-of-rust/lifetime-annotations/src/lib.rs
@@ -1,12 +1,12 @@
 
 #[derive(Debug)]
-pub struct StrSplit<'haystack, 'delimiter> {
+pub struct StrSplit<'haystack, D> {
     remainder: Option<&'haystack str>,
-    delimiter: &'delimiter str,
+    delimiter: D,
 }
 
-impl<'haystack, 'delimiter> StrSplit<'haystack, 'delimiter> {
-    pub fn new(haystack: &'haystack str, delimiter: &'delimiter str) -> Self {
+impl<'haystack, D> StrSplit<'haystack, D> {
+    pub fn new(haystack: &'haystack str, delimiter: D) -> Self {
         Self {
             remainder: Some(haystack),
             delimiter,
@@ -14,14 +14,20 @@ impl<'haystack, 'delimiter> StrSplit<'haystack, 'delimiter> {
     }
 }
 
-impl<'haystack> Iterator for StrSplit<'haystack, '_> {
+pub trait Delimiter {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)>;
+}
+
+impl<'haystack, D> Iterator for StrSplit<'haystack, D>
+where D: Delimiter,
+{
     type Item = &'haystack str;
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(ref mut remainder) = self.remainder {
-            if let Some(next_delim) = remainder.find(self.delimiter) {
-                let until_delimiter = &remainder[..next_delim];
+            if let Some((delim_start, delim_end)) = self.delimiter.find_next(remainder) {
+                let until_delimiter = &remainder[..delim_start];
                 println!("next called. got: {}", until_delimiter);
-                *remainder = &remainder[(next_delim + self.delimiter.len())..];
+                *remainder = &remainder[delim_end..];
                 Some(until_delimiter)
             } else {
                 self.remainder.take()
@@ -32,8 +38,32 @@ impl<'haystack> Iterator for StrSplit<'haystack, '_> {
     }
 }
 
+impl Delimiter for &str {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.find(self).map(|start| (start, start + self.len()))
+    }
+}
+
+impl Delimiter for char {
+    fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+        s.char_indices()
+            .find(|(_, c)| c == self)
+            .map(|(start, what)| {
+                println!("what is it? {}", what);
+                (start, start + self.len_utf8())
+            })
+    }
+}
+
 fn until_char(s: &str, c: char) -> &str {
-    StrSplit::new(s, &format!("{}", c))
+    StrSplit::new(s, &*format!("{}", c))
+        .next()
+        .expect("one result")
+}
+
+fn until_char_char(s: &str, c: char) -> &str {
+    println!("until_char_char_called");
+    StrSplit::new(s, c)
         .next()
         .expect("one result")
 }
@@ -41,6 +71,11 @@ fn until_char(s: &str, c: char) -> &str {
 #[test]
 fn until_char_test() {
     assert_eq!(until_char("hello world", 'o'), "hell");
+}
+
+#[test]
+fn until_char_char_test() {
+    assert_eq!(until_char_char("hello world", 'o'), "hell");
 }
 
 #[test]
