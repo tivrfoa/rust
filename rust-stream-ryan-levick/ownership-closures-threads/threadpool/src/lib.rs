4488c4f767c4010d0bae1090c69ed01b55bb1261
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-05-05 22:57:19 -0300

    Create member variable to handle the threads

--- rust-stream-ryan-levick/ownership-closures-threads/threadpool/src/lib.rs
+++ rust-stream-ryan-levick/ownership-closures-threads/threadpool/src/lib.rs
@@ -1,13 +1,30 @@
-pub struct ThreadPool;
+/**
+ *
+ * These are OS threads. Green threads were removed
+ * from Rust, because in order to have them you need
+ * to have a runtime, and that adds overhead that goes
+ * against the Rust philosophy of zero cost abstractions.
+ *
+ * Tokio tasks are green threads.
+ *
+ */
+pub struct ThreadPool {
+	_handles: Vec<std::thread::JoinHandle<()>>
+}
 
 impl ThreadPool {
-	pub fn new() -> Self {
-		Self
+	pub fn new(num_threads: u8) -> Self {
+		let _handles = (0..num_threads)
+			.map(|_| std::thread::spawn(|| {}))
+			.collect();
+		Self {
+			_handles
+		}
 	}
 
 	// needs & here, otherwise the first call
 	// to execute would move the ownership
-	pub fn execute(&self) {}
+	pub fn execute<T: Fn()>(&self, work: T) {}
 }
 
 #[cfg(test)]
@@ -16,7 +33,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-		let pool = ThreadPool::new();
-		pool.execute();
+		let pool = ThreadPool::new(10);
+		pool.execute(|| println!("Hello from thread"));
+		pool.execute(|| println!("Hello from thread"));
     }
 }
