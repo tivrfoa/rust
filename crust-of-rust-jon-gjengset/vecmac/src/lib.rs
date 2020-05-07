a0e2903a687921f3cfd89727c419b568191476ba
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-05-07 09:56:00 -0300

    Using ident (identifier) to receive name in macro

--- crust-of-rust-jon-gjengset/vecmac/src/lib.rs
+++ crust-of-rust-jon-gjengset/vecmac/src/lib.rs
@@ -1,17 +1,34 @@
 
 /**
+ * For this tutorial, is recommended to install the
+ * follow extension:
+ * $ cargo install cargo-expand
+ *
+ * All that matters for the input to a macro is that
+ * it can be parsed. And then what matters for compilation
+ * is whether the output is valid Rust, because the output
+ * is what actually gets compiled.
+ *
+ * Basically you can think of every macro invocation as being
+ * replaced by whatever its input was.
+ *
  * The identifiers in the macro world are just
  * completely distinct from the variables outside of
  * macro world.
  *
+ * Identifiers are used for passing names to macros.
  *
  */
 macro_rules! avec {
-	() => {};
+	($x:ident) => {
+		$x += 1;
+	};
 }
 
-/*
-avec!();
-avec![];
-avec!{}
-*/
+#[test]
+fn foo() {
+	let mut x = 42;
+	avec!(x);
+	println!("{}", x);
+	assert_eq!(x, 43);
+}
