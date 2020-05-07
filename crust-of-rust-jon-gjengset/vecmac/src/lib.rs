db5dc0fbae02f36493b9d24755e1a0ecc24909a0
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-05-07 09:12:56 -0300

    Learning macro: Crust of Rust: Declarative Macros

---
+++ crust-of-rust-jon-gjengset/vecmac/src/lib.rs
@@ -0,0 +1,17 @@
+
+/**
+ * The identifiers in the macro world are just
+ * completely distinct from the variables outside of
+ * macro world.
+ *
+ *
+ */
+macro_rules! avec {
+	() => {};
+}
+
+/*
+avec!();
+avec![];
+avec!{}
+*/
