a989924e47071826bb4efb7880ffecd83d1bec6a
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-06-11 11:24:27 -0300

    Reorder tree, gcd, Fenwick Tree, various utilities functions

---
+++ trees/ReorderRoutes.rs
@@ -0,0 +1,288 @@
+/*
+ * @lc app=leetcode id=1466 lang=rust
+ *
+ * [1466] Reorder Routes to Make All Paths Lead to the City Zero
+ *
+ * https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/description/
+ *
+ * algorithms
+ * Medium (63.68%)
+ * Total Accepted:    6.7K
+ * Total Submissions: 10.5K
+ * Testcase Example:  '6\n[[0,1],[1,3],[2,3],[4,0],[4,5]]'
+ *
+ * There are n cities numbered from 0 to n-1 and n-1 roads such that there is
+ * only one way to travel between two different cities (this network form a
+ * tree). Last year, The ministry of transport decided to orient the roads in
+ * one direction because they are too narrow.
+ * 
+ * Roads are represented by connections where connections[i] = [a, b]
+ * represents a road from city a to b.
+ * 
+ * This year, there will be a big event in the capital (city 0), and many
+ * people want to travel to this city.
+ * 
+ * Your task consists of reorienting some roads such that each city can visit
+ * the city 0. Return the minimum number of edges changed.
+ * 
+ * It's guaranteed that each city can reach the city 0 after reorder.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]
+ * Output: 3
+ * Explanation: Change the direction of edges show in red such that each node
+ * can reach the node 0 (capital).
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * 
+ * Input: n = 5, connections = [[1,0],[1,2],[3,2],[3,4]]
+ * Output: 2
+ * Explanation: Change the direction of edges show in red such that each node
+ * can reach the node 0 (capital).
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: n = 3, connections = [[1,0],[2,0]]
+ * Output: 0
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 2 <= n <= 5 * 10^4
+ * connections.length == n-1
+ * connections[i].length == 2
+ * 0 <= connections[i][0], connections[i][1] <= n-1
+ * connections[i][0] != connections[i][1]
+ * 
+ * 
+ */
+impl Solution {
+    pub fn min_reorder(n: i32, ref mut connections: Vec<Vec<i32>>) -> i32 {
+        let mut cnt = 0; 
+        let mut visited: Vec<bool> = vec![false; n as usize];
+        visited[0] = true; 
+        while ! connections.is_empty(){
+            let mut tmp = vec![];
+            for pair in connections.iter() {
+                let (a, b) = (pair[0] as usize ,pair[1] as usize);
+                if visited[a] {
+                    visited[b] = true; 
+                    cnt += 1; 
+                } else if visited[b] {
+                    visited[a] = true; 
+                } else {
+                    tmp.push(vec![a as i32, b as i32]);
+                }
+            }
+            *connections = tmp;
+        }
+        cnt 
+    }
+}
+
+fn main() {
+}
+
+
+pub struct Solution; 
+static CHARHASH: [i32; 26] = [-9536, -6688, 2006, -2069, 7302, -8825, -8832, 7678, 4540, 7567, 5286, 7027, -8601, -7555, -4541, 6134, 9023, 7805, -3888, 8309, -5265, 7487, -2988, 292, -5646, 7002];
+
+pub fn hash_string(s: String) -> i32 {
+    s.chars().map(|c| CHARHASH[char2usize(c)]).sum()
+}
+
+use std::cmp::max;
+use std::cmp::min;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fmt::Debug;
+use std::hash::Hash;
+use std::iter::FromIterator;
+// use std::collections::VecDeque;
+// use std::collections::BTreeMap;
+use std::any::type_name;
+use std::collections::BinaryHeap;
+
+pub fn char2usize(c:char) -> usize {
+    c as usize - 97
+}
+
+pub struct Helper;
+impl Helper {
+    pub fn stringify(str_vector: Vec<&str>) -> Vec<String> {
+        // Convert a vector of &str to vector or String for coding convenience
+        str_vector
+            .iter()
+            .map(|c| c.to_string())
+            .collect::<Vec<String>>()
+    }
+}
+
+// To get the type of a variable
+pub fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+#[allow(dead_code)]
+pub fn print_map<K: Debug + Eq + Hash, V: Debug>(map: &HashMap<K, V>) {
+    for (k, v) in map.iter() {
+        println!("{:?}: {:?}", k, v);
+    }
+}
+
+#[allow(dead_code)]
+pub fn say_vec(nums: Vec<i32>) {
+    println!("{:?}", nums);
+}
+
+#[allow(dead_code)]
+pub fn char_frequency(s: String) -> HashMap<char, i32> {
+    let mut res: HashMap<char, i32> = HashMap::new();
+    for c in s.chars() {
+        *res.entry(c).or_insert(0) += 1;
+    }
+    res
+}
+
+#[allow(dead_code)]
+pub fn string_counter(s: &str) -> HashMap<char, i32> {
+    let mut res = HashMap::new();
+    for c in s.chars() {
+        *res.entry(c).or_insert(0) += 1;
+    }
+    res
+}
+
+#[allow(dead_code)]
+pub fn vec_counter(arr: &Vec<i32>) -> HashMap<i32, i32> {
+    let mut c = HashMap::new();
+    for n in arr {
+        *c.entry(*n).or_insert(0) += 1;
+    }
+    c
+}
+
+#[allow(dead_code)]
+pub fn vec_to_hashset(arr: &Vec<i32>) -> HashSet<i32> {
+    HashSet::from_iter(arr.iter().cloned())
+}
+
+#[allow(dead_code)]
+pub fn int_to_char(n: i32) -> char {
+    // Convert number 0 to a, 1 to b, ...
+    assert!(n >= 0 && n <= 25);
+    (n as u8 + 'a' as u8) as char
+}
+
+#[allow(dead_code)]
+fn sayi32(i: i32) {
+    println!("{}", i);
+}
+
+#[allow(dead_code)]
+fn sayi32_arr(arr: &Vec<i32>) {
+    println!("{:?}", arr);
+}
+
+#[allow(dead_code)]
+pub fn bisect_left(arr: &Vec<i32>, target: i32) -> usize {
+    if target > *arr.last().unwrap() { return arr.len() }
+    let (mut lo, mut hi) = (0, arr.len() - 1);
+    let mut mid;
+    while lo < hi {
+        mid = (lo + hi) >> 1;
+        if arr[mid as usize] >= target {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    lo
+}
+
+#[allow(dead_code)]
+pub fn bisect_right(arr: &Vec<i32>, target: i32) -> usize {
+    let (mut lo, mut hi) = (0, arr.len() - 1);
+    let mut mid;
+    while lo < hi {
+        mid = (lo + hi + 1) >> 1;
+        if arr[mid as usize] > target {
+            hi = mid - 1;
+        } else {
+            lo = mid;
+        }
+    }
+    if arr[hi] > target {
+        hi
+    } else {
+        hi + 1
+    }
+}
+
+pub struct FenwickTree {
+    vals: Vec<i32>,
+}
+
+impl FenwickTree {
+    pub fn new(size: usize) -> FenwickTree {
+        FenwickTree {
+            vals: Vec::from_iter(std::iter::repeat(0).take(size + 1)),
+        }
+    }
+
+    pub fn update(&mut self, mut i: usize, val: i32) {
+        let size = self.vals.len();
+        while i < size {
+            self.vals[i] += val;
+            i += i & (!i + 1);
+        }
+    }
+
+    pub fn get(&mut self, mut i: usize) -> i32 {
+        let mut res = 0;
+        while i > 0 {
+            res += self.vals[i];
+            i -= i & (!i + 1);
+        }
+        res
+    }
+}
+
+#[allow(dead_code)]
+fn get_vector_sum(a: &Vec<i32>) -> i32 {
+    a.iter().fold(0, |mut sum, &x| {
+        sum += x;
+        sum
+    })
+}
+
+#[allow(dead_code)]
+fn get_vector_product(a: &Vec<i32>) -> i32 {
+    a.iter().fold(1, |mut prod, &x| {
+        prod *= x;
+        prod
+    })
+}
+
+// There is NO gcd in standard lib for Rust, surprise.  
+#[allow(dead_code)]
+fn gcd(a: i32, b: i32) -> i32 {
+    if b == 0 { a } else { gcd(b, a % b)}
+}
+
+#[allow(dead_code)]
+fn capitalize(word: String) -> String { 
+	let mut res = word;
+	res.chars().take(1).flat_map(char::to_uppercase).chain(res.chars().skip(1)).collect()
+}
