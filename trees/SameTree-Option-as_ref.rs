a4f970d4f2a391fb10c102c0d6ca637c57de5696
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-06-21 15:54:42 -0300

    Another approach to traverse a tree using Option as_ref

--- trees/SameTree-Option-as_ref.rs
+++ trees/SameTree-Option-as_ref.rs
@@ -23,14 +23,22 @@ impl TreeNode {
 struct Solution;
 
 impl Solution {
-    pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+	pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        Solution::is_same_tree_ref(p.as_ref(), q.as_ref())
+    }
+    pub fn is_same_tree_ref(p: Option<&Rc<RefCell<TreeNode>>>, q: Option<&Rc<RefCell<TreeNode>>>) -> bool {
         if p.is_none() && q.is_none() { return true; }
         if p.is_none() || q.is_none() { return false; }
-        let p = Rc::try_unwrap(p.unwrap()).unwrap().into_inner();
-        let q = Rc::try_unwrap(q.unwrap()).unwrap().into_inner();
+        let p = p.unwrap().borrow();
+        let q = q.unwrap().borrow();
         if p.val != q.val { return false; }
-        
-        Solution::is_same_tree(p.left, q.left) && Solution::is_same_tree(p.right, q.right)
+
+		println!("{:#?}", p);
+		println!("{:#?}", p.left);
+		println!("{:?}", p.left.as_ref());
+
+        Solution::is_same_tree_ref(p.left.as_ref(), q.left.as_ref()) &&
+        Solution::is_same_tree_ref(p.right.as_ref(), q.right.as_ref())
     }
 }
 
@@ -47,7 +55,19 @@ fn main() {
 	})));
 	assert_eq!(Solution::is_same_tree(p, q), true);
 
+	println!("=================================");
 	let t = new_node(5);
+
+	let tr = &t;
+	match tr {
+		Some(e) => {
+			println!("e = {:#?}", e);
+			println!("*e = {:#?}", *e);
+			println!("e.borrow() = {:#?}", e.borrow());
+		},
+		None => println!("None {:?}", tr),
+	}
+	println!("=================================");
 
 	println!("{:#?}", t);
 	let t = t.unwrap();
