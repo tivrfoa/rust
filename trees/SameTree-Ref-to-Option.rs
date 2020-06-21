c10477bf2c272bc2a9f8a093cfa48f425ca834a7
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-06-21 15:57:42 -0300

    traversing an tree passing an reference to an Option

---
+++ trees/SameTree-Ref-to-Option.rs
@@ -0,0 +1,86 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+struct Solution;
+
+impl Solution {
+	pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        Solution::is_same_tree_ref(&p, &q)
+    }
+    pub fn is_same_tree_ref(p: &Option<Rc<RefCell<TreeNode>>>, q: &Option<Rc<RefCell<TreeNode>>>) -> bool {
+        match (p, q)  {
+            (None, None) => true,
+            (Some(_), None) | (None, Some(_)) => false,
+            (Some(pp), Some(qq)) => {
+                let pp = pp.borrow();
+                let qq = qq.borrow();
+                if pp.val != qq.val { return false; }
+                Solution::is_same_tree_ref(&pp.left, &qq.left) &&
+                        Solution::is_same_tree_ref(&pp.right, &qq.right)
+            }
+        }
+    }
+}
+
+fn main() {
+	let p = Some(Rc::new(RefCell::new(TreeNode {
+		val: 1,
+		left: new_node(2),
+		right: new_node(3), 
+	})));
+	let q = Some(Rc::new(RefCell::new(TreeNode {
+		val: 1,
+		left: new_node(2),
+		right: new_node(3), 
+	})));
+	assert_eq!(Solution::is_same_tree(p, q), true);
+
+	println!("=================================");
+	let t = new_node(5);
+
+	let tr = &t;
+	match tr {
+		Some(e) => {
+			println!("e = {:#?}", e);
+			println!("*e = {:#?}", *e);
+			println!("e.borrow() = {:#?}", e.borrow());
+		},
+		None => println!("None {:?}", tr),
+	}
+	println!("=================================");
+
+	println!("{:#?}", t);
+	let t = t.unwrap();
+	println!("{:#?}", t);
+	let t = t.borrow();
+	println!("{:#?}", t);
+	println!("{:#?}", t.val);
+	println!("{:#?}", t.left);
+}
+
+fn new_node(val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+	Some(Rc::new(RefCell::new(TreeNode {
+		val,
+		left: None,
+		right: None,
+	})))
+}
